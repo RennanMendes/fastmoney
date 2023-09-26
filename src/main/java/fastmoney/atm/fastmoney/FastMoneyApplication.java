f6e43755be6c34283c6b771c30f569a9ed2850e3
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-09-26 00:08:05 -0300

    initial commit

---
+++ src/main/java/fastmoney/atm/fastmoney/FastMoneyApplication.java
@@ -0,0 +1,13 @@
+package fastmoney.atm.fastmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastMoneyApplication.class, args);
+	}
+
+}
