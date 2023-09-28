b3312a2f9c04ba8f4464e44087bb4dab4d34a224
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-09-28 16:15:25 -0300

    refactor: transaction service deposit and withdraw methods

--- src/main/java/fastmoney/atm/fastmoney/domain/service/TransactionService.java
+++ src/main/java/fastmoney/atm/fastmoney/domain/service/TransactionService.java
@@ -30,36 +30,29 @@ public class TransactionService {
     }
 
     public TransactionResponseDto deposit(Long id, TransactionRequestDto requestDto) {
-        TransactionType type =  TransactionType.INPUT;
         User user = userService.findByIdAndActiveTrue(id);
-        ValidationDto dto = new ValidationDto(user, requestDto.value(), requestDto.pin(), type);
-
-        validators.forEach(v -> v.validate(dto));
-
-        user.getAccount().calculateBalance(type, requestDto.value());
-        Transaction transaction = transaction(user, FinancialTransaction.DEPOSIT, type, requestDto.value());
+        this.validate(user, requestDto, TransactionType.INPUT);
+        Transaction transaction = transfer(user, FinancialTransaction.WITHDRAWAL, TransactionType.INPUT, requestDto.value());
 
         return new TransactionResponseDto(transaction);
     }
 
     public TransactionResponseDto withdraw(Long id, TransactionRequestDto requestDto) {
-        TransactionType type =  TransactionType.OUTPUT;
         User user = userService.findByIdAndActiveTrue(id);
-        ValidationDto dto = new ValidationDto(user, requestDto.value(), requestDto.pin(), type);
-
-        validators.forEach(v -> v.validate(dto));
-
-        user.getAccount().calculateBalance(type, requestDto.value());
-        Transaction transaction = transaction(user, FinancialTransaction.WITHDRAWAL, type, requestDto.value());
+        this.validate(user, requestDto, TransactionType.OUTPUT);
+        Transaction transaction = transfer(user, FinancialTransaction.WITHDRAWAL, TransactionType.OUTPUT, requestDto.value());
 
         return new TransactionResponseDto(transaction);
     }
 
-    private Transaction save(Transaction transactional) {
-        return repository.save(transactional);
+    private void validate(User user, TransactionRequestDto requestDto, TransactionType type) {
+        ValidationDto dto = new ValidationDto(user, requestDto.value(), requestDto.pin(), type);
+        validators.forEach(v -> v.validate(dto));
     }
 
-    private Transaction transaction(User user, FinancialTransaction financialTransaction, TransactionType type, BigDecimal value) {
-        return this.save(new Transaction(user, financialTransaction, type, value));
+    private Transaction transfer(User user, FinancialTransaction financialTransaction, TransactionType type, BigDecimal amount) {
+        user.getAccount().calculateBalance(type, amount);
+        return repository.save(new Transaction(user, financialTransaction, type, amount));
     }
+
 }
