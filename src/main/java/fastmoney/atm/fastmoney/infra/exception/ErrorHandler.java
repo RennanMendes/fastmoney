adf227b6c2b814583a36afa1b5b391ebdd3cce99
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-09-28 13:06:04 -0300

    feat: create error handler invalid transaction

--- src/main/java/fastmoney/atm/fastmoney/infra/exception/ErrorHandler.java
+++ src/main/java/fastmoney/atm/fastmoney/infra/exception/ErrorHandler.java
@@ -1,5 +1,8 @@
 package fastmoney.atm.fastmoney.infra.exception;
 
+import fastmoney.atm.fastmoney.domain.exception.InvalidBalanceException;
+import fastmoney.atm.fastmoney.domain.exception.InvalidPinException;
+import fastmoney.atm.fastmoney.domain.exception.InvalidValueException;
 import fastmoney.atm.fastmoney.domain.exception.UserNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +14,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity handleUserNotFoundException(){
-        return ResponseEntity.notFound().build();
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity handleError400(MethodArgumentNotValidException exception) {
         var errors = exception.getFieldErrors();
@@ -26,6 +24,26 @@ public class ErrorHandler {
     public ResponseEntity handleDataIntegrityViolationException(DataIntegrityViolationException exception){
         var error = exception.getMessage();
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity handleUserNotFoundException(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(InvalidBalanceException.class)
+    public ResponseEntity handleInvalidBalanceException(InvalidBalanceException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPinException.class)
+    public ResponseEntity handleInvalidPinException(InvalidPinException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidValueException.class)
+    public ResponseEntity handleInvalidValueException(InvalidValueException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     private record validationDataError(String field, String message) {
