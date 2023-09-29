3454f4a0864d07b390827a04846b70992be58b17
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-09-29 19:13:14 -0300

    feat: create transfer and statement methods in transaction service

--- src/main/java/fastmoney/atm/fastmoney/api/controller/TransactionController.java
+++ src/main/java/fastmoney/atm/fastmoney/api/controller/TransactionController.java
@@ -5,6 +5,8 @@ import fastmoney.atm.fastmoney.domain.dto.transaction.TransactionRequestDto;
 import fastmoney.atm.fastmoney.domain.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +26,19 @@ public class TransactionController {
     @PostMapping("/withdrawals/{id}")
     public ResponseEntity<TransactionResponseDto> withdraw(@PathVariable Long id, @RequestBody @Valid TransactionRequestDto requestDto) {
         TransactionResponseDto responseDto = transactionService.withdraw(id, requestDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @PostMapping("/transfers/{senderId}/{receiverId}")
+    public ResponseEntity<TransactionResponseDto> transfer(@PathVariable Long senderId, @PathVariable Long receiverId,
+                                                           @RequestBody @Valid TransactionRequestDto requestDto) {
+        TransactionResponseDto responseDto = transactionService.transfer(senderId, receiverId, requestDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Page<TransactionResponseDto>> statement(@PathVariable Long id, Pageable page) {
+        Page<TransactionResponseDto> responseDto = transactionService.statement(id, page);
         return ResponseEntity.ok().body(responseDto);
     }
 
--- src/main/java/fastmoney/atm/fastmoney/domain/repository/TransactionRepository.java
+++ src/main/java/fastmoney/atm/fastmoney/domain/repository/TransactionRepository.java
@@ -1,7 +1,16 @@
 package fastmoney.atm.fastmoney.domain.repository;
 
 import fastmoney.atm.fastmoney.domain.model.Transaction;
+import fastmoney.atm.fastmoney.domain.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("SELECT t FROM Transaction t WHERE t.fromAccount = :id ORDER BY t.date ASC")
+    Page<Transaction> findByFromAccountId(User id, Pageable pageable);
+
 }
+
